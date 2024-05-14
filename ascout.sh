944341a5b8dad43418a7ece3a3c8510d184dcdb9
Ali İrfan Doğan <alirfandogan@gmail.com>
2024-05-15 00:54:59 +0300

    version 1.2

--- ascout.sh
+++ ascout.sh
@@ -1,30 +1,83 @@
-#! /bin/bash
-# ./ascout <ip_address> <output_file> <y/n>
-# created by @cozuxhub
+#!/usr/bin/env bash
+# ./aScout <ip_address> <output_file> <y/n>
+# created by @norcholly
+# required tools: gnome-terminal, gobuster
+# alirfandogan.com
 
 clear
-echo -e "\033[92m"
-echo "aScout v1.1 By @cozuxhub" | figlet -f small
+echo -e "\033[92maScout v1.2 By \033[1m@norcholly\033[0m\n"
 
 # Let's assign the entered arguments to variables.
 ipaddr=$1
 output=$2
 verify=$3
+order_of_the_code=1
 
 # Exit the script if the arguments are not entered correctly, and prompt the user to run the script again.
 if [[ -z "$ipaddr" ]]; then
-	echo -e "\n\033[1m\033[91mUse: $0 <IP Address> <Output Text File>\n\n\n"
-	exit 1
+        echo -e "\n\033[1m\033[91mUse: $0 <IP Address> <Output Text File>"
+        exit 1
 fi
 
 if [[ -z "$output" ]]; then
-	echo -e "\n\033[1m\033[91mUse: $0 <IP Address> <Output Text File>\n\n\n"
-	exit 1
+        echo -e "\n\033[1m\033[91mUse: $0 <IP Address> <Output Text File>"
+        exit 1
 fi
+
+function essentials() {
+	# Let's check if Gnome Terminal is installed.
+	if ! command -v gnome-terminal &> /dev/null; then
+		echo -e "\033[91mGnome Terminal is not found. The script requires Gnome Terminal to be installed.\033[92m"
+		read -p "Would you like to install Gnome Terminal? (y/n): " tdownload
+		tdownload=$(echo "$tdownload" | tr '[:upper:]' '[:lower:]')
+		if [[ $tdownload != "y" ]]; then
+			echo -e "\n\033[1m\033[96m[\033[0mX\033[96m]\033[91m The script will not run without Gnome Terminal!"
+			exit 1
+		else
+			echo -e "\033[1m\033[96m[\033[0m$order_of_the_code\033[1m\033[96m]\033[93m Installing Gnome-Terminal..."
+			sudo apt-get install gnome-terminal -y -qq > /dev/null 2>&1
+			echo -e "\033[1m\033[96m[\033[0m✓\033[1m\033[96m]\033[92m Gnome-Terminal has been successfully installed!\n\033[0m"
+			((order_of_the_code++))
+		fi
+	fi
+
+	# Let's check if Gobuster is installed.
+	if ! command -v gobuster &> /dev/null; then
+		echo -e "\033[91mGobuster is not found. The script requires Gobuster to be installed.\033[92m"
+		read -p "Would you like to install Gobuster? (y/n): " gdownload
+		gdownload=$(echo "$gdownload" | tr '[:upper:]' '[:lower:]')
+		if [[ $gdownload != "y" ]]; then
+			echo -e "\n\033[1m\033[96m[\033[0mX\033[96m]\033[91m The script will not run without Gobuster!"
+			exit 1
+		else
+			echo -e "\033[1m\033[96m[\033[0m$order_of_the_code\033[1m\033[96m]\033[93m Installing Gobuster..."
+			sudo apt-get install gobuster -y -qq > /dev/null 2>&1
+			echo -e "\033[1m\033[96m[\033[0m✓\033[1m\033[96m]\033[92m Gobuster has been successfully installed!"
+			((order_of_the_code++))
+			sleep 2
+			clear
+		fi
+	fi
+}
+
+function verify(){
+        # Let's remind the user that Active Reconnaissance will establish communication with the target again. If the user wants to skip this>
+        echo -e "\033[1m\033[91m\033[3mActive Reconnaissance involves actively probing a target to gather information, which could potentially pose security risks if done without proper authorization. This process includes scanning networks, identifying vulnerabilities, and gathering intelligence, which, if exploited by malicious actors, could lead to unauthorized access or data breaches.\033[0m\n"
+        if [[ $verify != "y" ]]; then
+                read -p "Are you sure you want to do 'Active Reconnaissance' to ? (y/n): " verify
+                verify=$(echo "$verify" | tr '[:upper:]' '[:lower:]')
+		if [[ $verify != "y" ]]; then
+			exit 1
+		fi
+        fi
+}
 
 # Let's first check if we can communicate with the target.
 function ping_checker(){
-	echo -e "\n\033[1m\033[96m[\033[0m1\033[96m\033[1m] \033[93mICMP packets are being sent to the target. Please wait...\n\033[0m"
+	clear
+	echo -e "\n\033[92m"
+	echo "aScout v1.2 by @norcholly" | figlet -f small
+	echo -e "\n\n\033[1m\033[96m[\033[0m$order_of_the_code\033[96m\033[1m] \033[93mICMP packets are being sent to the target. Please wait...\n\033[0m"
 	# Let the fifteen-minute timer begin.
 	timeout=900 # 15 min
 	start_time=$(date +%s)
@@ -60,27 +113,20 @@ function ping_checker(){
 }
 
 function areconnaissence(){
-	# Let's remind the user that Active Reconnaissance will establish communication with the target again. If the user wants to skip this step, they can add the letter "y" as the third argument.
-	echo -e "\033[1m\033[91m\033[3mActive Reconnaissance involves actively probing a target to gather information, which could potentially pose security risks if done without proper authorization. This process includes scanning networks, identifying vulnerabilities, and gathering intelligence, which, if exploited by malicious actors, could lead to unauthorized access or data breaches.\033[0m\n"
-	if [[ $verify != "y" ]]; then
-		read -p "Are you sure you want to do 'Active Reconnaissance' to ? (y/n): " verify
-		verify=$(echo "$verify" | tr '[:upper:]' '[:lower:]')
-	fi
-	if [[ $verify == "y" ]]; then
-		echo -e "\033[91m"
-		echo -e "Target: $ipaddr" | figlet -f small
-		echo -e "@cozuxhub" | figlet -f small
-		sleep 2
+	echo -e "\033[91m"
+	echo -e "Target: $ipaddr" | figlet -f small
+	echo -e "@norcholly" | figlet -f small
+	sleep 2
 
-		# At this point, let's open four separate terminals and run different active scanning tools in each terminal.
-		gnome-terminal --geometry=117x25+1000-1000 -- bash -c "echo -e '\033[1m'; dirb http://$ipaddr 2>&1 | tee $output; exec bash"
-		gnome-terminal --geometry=117x25+1000+1000 -- bash -c "echo -e '\033[1m'; nmap -A -p- -T4 $ipaddr -oN $output; exec bash"
-		gnome-terminal --geometry=117x25+0+0 -- bash -c "echo -e '\033[1m'; nikto -h $ipaddr ; exec bash"
-		gnome-terminal --geometry=117x25+0-0 -- bash -c "echo -e '\033[1m'; gobuster dir -u http://$ipaddr/ -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-medium.txt -x php,txt,html -o $output; exec bash"
-	else
-		exit 1
-	fi
+	# At this point, let's open four separate terminals and run different active scanning tools in each terminal.
+	gnome-terminal --geometry=117x25+1000-1000 -- bash -c "echo -e '\033[1m'; dirb http://$ipaddr 2>&1 | tee $output; exec bash"
+	gnome-terminal --geometry=117x25+1000+1000 -- bash -c "echo -e '\033[1m'; nmap -A -p- -T4 $ipaddr -oN $output; exec bash"
+	gnome-terminal --geometry=117x25+0+0 -- bash -c "echo -e '\033[1m'; nikto -h $ipaddr ; exec bash"
+	gnome-terminal --geometry=117x25+0-0 -- bash -c "echo -e '\033[1m'; gobuster dir -u http://$ipaddr/ -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-medium.txt -x php,html,asp,db,sqlite,sql,mdb,key,log,sh,tar,gz,bz2,zip,rar,7z,jpg,jpeg,png,txt,doc -o $output; exec bash"
+
 }
 
+essentials
+verify
 ping_checker
 areconnaissence
