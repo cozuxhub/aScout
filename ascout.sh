9f1126deb3ad9f518dd695b0f37c21bf010b9287
Ali İrfan Doğan <alirfandogan@gmail.com>
2024-05-28 11:37:17 +0300

    version 1.3

--- ascout.sh
+++ ascout.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env bash
-# ./aScout <ip_address> <output_file> <y/n>
+# ./aScout <ip_address> <output_file> <1-5> <y/n>
 # created by @norcholly
-# required tools: gnome-terminal, gobuster
+# required tools: gnome-terminal, gobuster, nmap, nikto
 # alirfandogan.com
+# v1.3
 
 clear
-echo -e "\033[92maScout v1.2 By \033[1m@norcholly\033[0m\n"
+echo -e "\033[92maScout v1.3 By \033[1m@norcholly\033[0m\n"
 
 # Let's assign the entered arguments to variables.
 ipaddr=$1
 output=$2
-verify=$3
+speed=$3
+verify=$4
 order_of_the_code=1
 
 # Exit the script if the arguments are not entered correctly, and prompt the user to run the script again.
 if [[ -z "$ipaddr" ]]; then
-        echo -e "\n\033[1m\033[91mUse: $0 <IP Address> <Output Text File>"
+        echo -e "\n\033[1m\033[91mUse: $0 <IP Address> <Output Text File> <Speed Range 1-5 (5 is very fast!)>"
         exit 1
 fi
 
 if [[ -z "$output" ]]; then
-        echo -e "\n\033[1m\033[91mUse: $0 <IP Address> <Output Text File>"
+        echo -e "\n\033[1m\033[91mUse: $0 <IP Address> <Output Text File> <Speed Range 1-5 (5 is very fast!)>"
         exit 1
+fi
+
+if [[ -z "$speed" ]]; then
+		echo -e "\n\033[1m\033[91mUse: $0 <IP Address> <Output Text File> <Speed Range 1-5 (5 is very fast!)>"
+		exit 1
 fi
 
 function essentials() {
@@ -34,7 +41,7 @@ function essentials() {
 			echo -e "\n\033[1m\033[96m[\033[0mX\033[96m]\033[91m The script will not run without Gnome Terminal!"
 			exit 1
 		else
-			echo -e "\033[1m\033[96m[\033[0m$order_of_the_code\033[1m\033[96m]\033[93m Installing Gnome-Terminal..."
+			echo -e "\033[1m\033[96m[\033[0m$order_of_the_code\033[1m\033[96m]\033[93m Installing Gnome-Terminal\033[5m...\033[0m"
 			sudo apt-get install gnome-terminal -y -qq > /dev/null 2>&1
 			echo -e "\033[1m\033[96m[\033[0m✓\033[1m\033[96m]\033[92m Gnome-Terminal has been successfully installed!\n\033[0m"
 			((order_of_the_code++))
@@ -50,9 +57,45 @@ function essentials() {
 			echo -e "\n\033[1m\033[96m[\033[0mX\033[96m]\033[91m The script will not run without Gobuster!"
 			exit 1
 		else
-			echo -e "\033[1m\033[96m[\033[0m$order_of_the_code\033[1m\033[96m]\033[93m Installing Gobuster..."
+			echo -e "\033[1m\033[96m[\033[0m$order_of_the_code\033[1m\033[96m]\033[93m Installing Gobuster\033[5m...\033[0m"
 			sudo apt-get install gobuster -y -qq > /dev/null 2>&1
 			echo -e "\033[1m\033[96m[\033[0m✓\033[1m\033[96m]\033[92m Gobuster has been successfully installed!"
+			((order_of_the_code++))
+			sleep 2
+			clear
+		fi
+	fi
+
+	# Let's check if nmap is installed.
+	if ! command -v nmap &> /dev/null; then
+		echo -e "\033[91mNmap is not found. The script requires Nmap to be installed.\033[92m"
+		read -p "Would you like to install Nmap? (y/n): " ndownload
+		ndownload=$(echo "$ndownload" | tr '[:upper:]' '[:lower:]')
+		if [[ $ndownload != "y" ]]; then
+			echo -e "\n\033[1m\033[96m[\033[0mX\033[96m]\033[91m The script will not run without Nmap!"
+			exit 1
+		else
+			echo -e "\033[1m\033[96m[\033[0m$order_of_the_code\033[1m\033[96m]\033[93m Installing Nmap\033[5m...\033[0m"
+			sudo apt-get install nmap -y -qq > /dev/null 2>&1
+			echo -e "\033[1m\033[96m[\033[0m✓\033[1m\033[96m]\033[92m Nmap has been successfully installed!"
+			((order_of_the_code++))
+			sleep 2
+			clear
+		fi
+	fi
+
+	# Let's check if nikto is installed.
+	if ! command -v nikto &> /dev/null; then
+		echo -e "\033[91mNikto is not found. The script requires Nikto to be installed.\033[92m"
+		read -p "Would you like to install Nikto? (y/n): " nidownload
+		nidownload=$(echo "$nidownload" | tr '[:upper:]' '[:lower:]')
+		if [[ $nidownload != "y" ]]; then
+			echo -e "\n\033[1m\033[96m[\033[0mX\033[96m]\033[91m The script will not run without Nikto!"
+			exit 1
+		else
+			echo -e "\033[1m\033[96m[\033[0m$order_of_the_code\033[1m\033[96m]\033[93m Installing Nikto\033[5m...\033[0m"
+			sudo apt-get install nikto -y -qq > /dev/null 2>&1
+			echo -e "\033[1m\033[96m[\033[0m✓\033[1m\033[96m]\033[92m Nikto has been successfully installed!"
 			((order_of_the_code++))
 			sleep 2
 			clear
@@ -62,7 +105,7 @@ function essentials() {
 
 function verify(){
         # Let's remind the user that Active Reconnaissance will establish communication with the target again. If the user wants to skip this>
-        echo -e "\033[1m\033[91m\033[3mActive Reconnaissance involves actively probing a target to gather information, which could potentially pose security risks if done without proper authorization. This process includes scanning networks, identifying vulnerabilities, and gathering intelligence, which, if exploited by malicious actors, could lead to unauthorized access or data breaches.\033[0m\n"
+        echo -e "\033[1m\033[91m\033[3mActive Reconnaissance involves actively probing a target to gather information, which could potentially pose \033[5msecurity risks\033[0m\033[1m\033[91m\033[3m if done without proper authorization. This process includes scanning networks, identifying vulnerabilities, and gathering intelligence, which, if exploited by malicious actors, could lead to unauthorized access or data breaches.\033[0m\n"
         if [[ $verify != "y" ]]; then
                 read -p "Are you sure you want to do 'Active Reconnaissance' to ? (y/n): " verify
                 verify=$(echo "$verify" | tr '[:upper:]' '[:lower:]')
@@ -76,8 +119,8 @@ function verify(){
 function ping_checker(){
 	clear
 	echo -e "\n\033[92m"
-	echo "aScout v1.2 by @norcholly" | figlet -f small
-	echo -e "\n\n\033[1m\033[96m[\033[0m$order_of_the_code\033[96m\033[1m] \033[93mICMP packets are being sent to the target. Please wait...\n\033[0m"
+	echo "aScout v1.3 by @norcholly" | figlet -f small
+	echo -e "\n\n\033[1m\033[96m[\033[0m$order_of_the_code\033[96m\033[1m] \033[93mICMP packets are being sent to the target. Please wait\033[5m...\n\033[0m"
 	# Let the fifteen-minute timer begin.
 	timeout=900 # 15 min
 	start_time=$(date +%s)
@@ -106,7 +149,7 @@ function ping_checker(){
 			# If there was no loss in any of the three ICMP packets sent, our communication with the target is strong. Let's terminate the while.
 		fi
 
-		echo -e "\n\033[1m\033[96m[\033[0m$packet_number\033[1m\033[96m] \033[93mWaiting for ping response...\n -> Elapsed time: $elapsed_time\033[0m\n"
+		echo -e "\n\033[1m\033[96m[\033[0m$packet_number\033[1m\033[96m] \033[93mWaiting for ping response\033[5m...\033[0m\033[93m\n -> Elapsed time: $elapsed_time\033[0m\n"
 		sleep 4
 		((packet_number++))
 	done
@@ -118,12 +161,33 @@ function areconnaissence(){
 	echo -e "@norcholly" | figlet -f small
 	sleep 2
 
-	# At this point, let's open four separate terminals and run different active scanning tools in each terminal.
-	gnome-terminal --geometry=117x25+1000-1000 -- bash -c "echo -e '\033[1m'; dirb http://$ipaddr 2>&1 | tee $output; exec bash"
-	gnome-terminal --geometry=117x25+1000+1000 -- bash -c "echo -e '\033[1m'; nmap -A -p- -T4 $ipaddr -oN $output; exec bash"
-	gnome-terminal --geometry=117x25+0+0 -- bash -c "echo -e '\033[1m'; nikto -h $ipaddr ; exec bash"
-	gnome-terminal --geometry=117x25+0-0 -- bash -c "echo -e '\033[1m'; gobuster dir -u http://$ipaddr/ -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-medium.txt -x php,html,asp,db,sqlite,sql,mdb,key,log,sh,tar,gz,bz2,zip,rar,7z,jpg,jpeg,png,txt,doc -o $output; exec bash"
-
+	# At this point, let's open four separate terminals and run different active scanning tools in each terminal. The type of scan you perform will be shaped by the speed input provided.
+	if [[ $speed == "1" ]]; then
+		gnome-terminal --geometry=117x25+1000-1000 -- bash -c "echo -e '\033[1m'; dirb http://$ipaddr 2>&1 | tee $output; exec bash"
+		gnome-terminal --geometry=117x25+1000+1000 -- bash -c "echo -e '\033[1m'; nmap -A -p- -T4 $ipaddr -oN $output; exec bash"
+		gnome-terminal --geometry=117x25+0+0 -- bash -c "echo -e '\033[1m'; nikto -h $ipaddr ; exec bash"
+		gnome-terminal --geometry=117x25+0-0 -- bash -c "echo -e '\033[1m'; gobuster dir -u http://$ipaddr/ -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-medium.txt -x php,html,asp,db,sqlite,sql,mdb,key,log,sh,tar,gz,bz2,zip,rar,7z,jpg,jpeg,png,txt,doc -o $output; exec bash"
+	elif [[ $speed == "2" ]]; then
+		gnome-terminal --geometry=117x25+1000-1000 -- bash -c "echo -e '\033[1m'; dirb http://$ipaddr 2>&1 | tee $output; exec bash"
+		gnome-terminal --geometry=117x25+1000+1000 -- bash -c "echo -e '\033[1m'; nmap -A -p- -T4 $ipaddr -oN $output; exec bash"
+		gnome-terminal --geometry=117x25+0+0 -- bash -c "echo -e '\033[1m'; nikto -h $ipaddr ; exec bash"
+		gnome-terminal --geometry=117x25+0-0 -- bash -c "echo -e '\033[1m'; gobuster dir -u http://$ipaddr/ -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -x php,html,asp,db,sqlite,sql,mdb,key,log,sh,tar,gz,bz2,zip,rar,7z,jpg,jpeg,png,txt,doc -o $output; exec bash"
+	elif [[ $speed == "3" ]]; then
+		gnome-terminal --geometry=117x25+1000-1000 -- bash -c "echo -e '\033[1m'; dirb http://$ipaddr 2>&1 | tee $output; exec bash"
+		gnome-terminal --geometry=117x25+1000+1000 -- bash -c "echo -e '\033[1m'; nmap -A -p- -T4 $ipaddr -oN $output; exec bash"
+		gnome-terminal --geometry=117x25+0+0 -- bash -c "echo -e '\033[1m'; nikto -h $ipaddr ; exec bash"
+		gnome-terminal --geometry=117x25+0-0 -- bash -c "echo -e '\033[1m'; gobuster dir -u http://$ipaddr/ -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -o $output; exec bash"
+	elif [[ $speed == "4" ]]; then
+		gnome-terminal --geometry=117x25+1000-1000 -- bash -c "echo -e '\033[1m'; dirb http://$ipaddr 2>&1 | tee $output; exec bash"
+		gnome-terminal --geometry=117x25+1000+1000 -- bash -c "echo -e '\033[1m'; nmap -A -T4 $ipaddr -oN $output; exec bash"
+		gnome-terminal --geometry=117x25+0+0 -- bash -c "echo -e '\033[1m'; nikto -h $ipaddr ; exec bash"
+		gnome-terminal --geometry=117x25+0-0 -- bash -c "echo -e '\033[1m'; gobuster dir -u http://$ipaddr/ -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -o $output; exec bash"
+	elif [[ $speed == "5" ]]; then
+		gnome-terminal --geometry=117x25+1000-1000 -- bash -c "echo -e '\033[1m'; dirb http://$ipaddr 2>&1 | tee $output; exec bash"
+		gnome-terminal --geometry=117x25+1000+1000 -- bash -c "echo -e '\033[1m'; nmap -p- $ipaddr -vvv -oN $output; exec bash"
+		gnome-terminal --geometry=117x25+0+0 -- bash -c "echo -e '\033[1m'; nikto -h $ipaddr ; exec bash"
+		gnome-terminal --geometry=117x25+0-0 -- bash -c "echo -e '\033[1m'; gobuster dir -u http://$ipaddr/ -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -o $output; exec bash"
+	fi
 }
 
 essentials
